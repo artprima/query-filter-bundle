b0208db2a8dd252f13501ab71d34ecd82422052f
Denis Voytyuk <denis.voytyuk@feedo.cz>
2018-03-08 08:55:33 +0100

    QueryFilterListener fully tested

--- Tests/EventListener/QueryFilterListenerTest.php
+++ Tests/EventListener/QueryFilterListenerTest.php
@@ -56,4 +56,94 @@ class QueryFilterListenerTest extends TestCase
         $listener = new QueryFilterListener($queryFilter);
         $listener->onKernelView($event);
     }
+
+    /**
+     * @test
+     */
+    public function onKernelView_should_do_nothing_on_queryfilter_attribute_not_set()
+    {
+        $response = self::getMockBuilder(ResponseInterface::class)
+            ->getMock();
+
+        $config = self::getMockBuilder(ConfigInterface::class)
+            ->getMock();
+
+        $queryFilter = self::getMockBuilder(QueryFilter::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $queryFilter
+            ->expects(self::never())
+            ->method('getData')
+            ->with($config)
+            ->willReturn($response);
+
+        $event = self::getMockBuilder(GetResponseForControllerResultEvent::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $request = new Request();
+        $event
+            ->expects(self::once())
+            ->method('getRequest')
+            ->willReturn($request);
+
+        $event
+            ->expects(self::never())
+            ->method('getControllerResult')
+            ->willReturn($config);
+
+        $event
+            ->expects(self::never())
+            ->method('setControllerResult')
+            ->with($response);
+
+        $listener = new QueryFilterListener($queryFilter);
+        $listener->onKernelView($event);
+    }
+
+    /**
+     * @test
+     */
+    public function onKernelView_should_do_nothing_on_controller_result_not_instance_of_ConfigInterface()
+    {
+        $response = self::getMockBuilder(ResponseInterface::class)
+            ->getMock();
+
+        $config = 'idiocracy';
+
+        $queryFilter = self::getMockBuilder(QueryFilter::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $queryFilter
+            ->expects(self::never())
+            ->method('getData')
+            ->with($config)
+            ->willReturn($response);
+
+        $event = self::getMockBuilder(GetResponseForControllerResultEvent::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $request = new Request();
+        $request->attributes->set('_queryfilter', true);
+        $event
+            ->expects(self::once())
+            ->method('getRequest')
+            ->willReturn($request);
+
+        $event
+            ->expects(self::once())
+            ->method('getControllerResult')
+            ->willReturn($config);
+
+        $event
+            ->expects(self::never())
+            ->method('setControllerResult')
+            ->with($response);
+
+        $listener = new QueryFilterListener($queryFilter);
+        $listener->onKernelView($event);
+    }
 }
