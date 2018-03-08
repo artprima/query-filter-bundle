baa89cf728fbc662a4c167be24e88f00aebe1500
Denis Voytyuk <denis.voytyuk@feedo.cz>
2018-03-08 14:10:15 +0100

    Fix improper namespaces

--- DependencyInjection/ArtprimaQueryFilterExtension.php
+++ DependencyInjection/ArtprimaQueryFilterExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Feedo\QueryFilterBundle\DependencyInjection;
+namespace Artprima\QueryFilterBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
  */
-class FeedoQueryFilterExtension extends Extension
+class ArtprimaQueryFilterExtension extends Extension
 {
     /**
      * {@inheritdoc}
