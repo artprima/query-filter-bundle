aca5b131c7acdd50ee9cf91f47be34ca715f6498
Denis Voytyuk <denis.voytyuk@feedo.cz>
2018-03-08 13:36:30 +0100

    Add @QueryFilter annotation

---
+++ Controller/Annotations/QueryFilter.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Artprima\QueryFilterBundle\Controller\Annotations;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * Class QueryFilter
+ *
+ * @author Denis Voytyuk <denis@voituk.ru>
+ *
+ * @package Artprima\QueryFilterBundle\Controller\Annotations
+ */
+class QueryFilter extends Template
+{
+    protected $serializedData = false;
+    /**
+     * @return string
+     */
+    public function getAliasName()
+    {
+        return 'queryfilter';
+    }
+
+    /**
+     * @param bool $serializedData
+     */
+    public function setSerializedData($serializedData)
+    {
+        $this->serializedData = $serializedData;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasSerializedData()
+    {
+        return $this->serializedData;
+    }
+}
