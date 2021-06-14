a4fb99abeda26a5775db82ab630499fb405daf59
Denis Voytyuk <denisvmedia@users.noreply.github.com>
2021-06-14 12:55:33 +0200

    Fix tests

--- .integration/symfony/config/bundles.php
+++ .integration/symfony/config/bundles.php
@@ -2,5 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Artprima\QueryFilterBundle\ArtprimaQueryFilterBundle::class => ['all' => true],
 ];
